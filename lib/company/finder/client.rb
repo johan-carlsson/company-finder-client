b7e01731c8d403f60f75c43a315344c6829a0cf3
Johan Carlsson <johancarlsson@me.com>
2014-03-02 11:02:02 +0100

    Added files generated by bundler

---
+++ lib/company/finder/client.rb
@@ -0,0 +1,9 @@
+require "company/finder/client/version"
+
+module Company
+  module Finder
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/company/finder/client/version.rb
@@ -0,0 +1,7 @@
+module Company
+  module Finder
+    module Client
+      VERSION = "0.0.1"
+    end
+  end
+end
